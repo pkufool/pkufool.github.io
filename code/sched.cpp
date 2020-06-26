2272064016de08eb0ca4a033b1141cd03b636bde
kingway <njukingway@gmail.com>
2020-06-27 00:00:55 +0800

    Site updated: 2020-06-27 00:00:55

---
+++ code/sched.cpp
@@ -0,0 +1,111 @@
+#include <pthread.h>
+#include <string.h>
+#include <sys/resource.h>
+
+#include<cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Thread {
+ public:
+  Thread() {}
+  ~Thread() {}
+
+  void Start() {
+    const int err = pthread_create(&tid_, NULL, Thread::RunThread, this);
+    if (err != 0) {
+      std::cout << "Thread create failed: " << strerror(err);
+      return;
+    }
+  }
+
+  void Run() {
+    int result = 0;
+    for (int i = 0; i < 1000; ++i) {
+      for (int j = i; j < 1000; ++j) {
+        result += log(i) + log(j);
+      }
+    }
+    std::cout << "Final result for thread " << pthread_self()
+              << " is " << result;
+  }
+  
+  void SetName(const std::string& name) {
+    int res = pthread_setname_np(tid_, name.c_str());
+    if (res != 0) {
+      std::cout << "Thread " << tid_ << " setname failed: "
+                << strerror(res) << std::endl;
+    }
+  }
+
+  void SetNiceValue(const int nice) {
+    nice_ = nice;
+  }
+
+  void BindCpu(const std::vector<int>& cpus) {
+    if (cpus.empty()) {
+      return;
+    }
+    cpu_set_t cpuset;
+    CPU_ZERO(&cpuset);
+    for (const auto& id : cpus) {
+      CPU_SET(id, &cpuset);
+    }
+    std::cout << "set cpu count: " << CPU_COUNT(&cpuset)
+              << " for thread:" << tid_;
+    int ret = pthread_setaffinity_np(tid_, sizeof(cpuset), &cpuset);
+    if (ret != 0) {
+      std::cout << "Failed to call pthread_setaffinity_np.";
+    }
+  }
+
+  void SetSchedulerPolicy(const std::string& policy, const int priority) {
+    struct sched_param param;
+    int policy_type = SCHED_OTHER;
+    if (policy == "SCHED_OTHER") {
+      policy_type = SCHED_OTHER;
+    } else if (policy == "SCHED_FIFO") {
+      policy_type = SCHED_FIFO;
+    } else if (policy == "SCHED_RR") {
+      policy_type = SCHED_RR;
+    } else if (policy == "SCHED_BATCH") {
+      policy_type = SCHED_BATCH;
+    } else if (policy == "SCHED_IDLE") {
+      policy_type = SCHED_IDLE;
+    }
+
+    int old_policy, old_priority;
+    pthread_getschedparam(tid_, &old_policy, &param);
+    old_priority = param.sched_priority;
+    param.sched_priority = priority;
+
+    int s = pthread_setschedparam(tid_, policy_type, &param);
+    if (s != 0)
+      std::cout << "pthread_setschedparam failed, return value : "
+                << strerror(s);
+    std::cout << "old policy : " << old_policy
+              << ", old priority : " << old_priority
+              << ", new policy : " << policy_type
+              << ", priority : " << priority;
+  }
+
+ private:
+  static void* RunThread(void *v) {
+    Thread* t = static_cast<Thread*>(v);
+
+    int ret = setpriority(PRIO_PROCESS, 0, t->nice_);
+    if (ret != 0) {
+      std::cout << "Setpriority for thread " << t->tid_
+                << " failed: " << strerror(ret) << std::endl;
+    } else {
+      std::cout << "Setpriority for thread " << t->tid_
+                << " to " << t->nice_ << std::endl;
+    }
+    t->Run();
+    return 0;
+  }
+
+  pthread_t tid_ = 0;
+  int nice_ = 0;
+};
